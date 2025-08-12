ca5e6b498fef079a90af6714f3b4e8d90dea010a
kullampallep <pkullampalle@gmail.com>
2025-08-12 09:51:44 -0400

    fixed posthog hopefully

--- src/app/posthog-init.tsx
+++ src/app/posthog-init.tsx
@@ -4,7 +4,10 @@ import posthog from "posthog-js";
 
 export function PostHogInit() {
   useEffect(() => {
-    posthog.init('phx_5dJFrBdgyLUbZ3H9YDpJyqBCe1Xo5TwOsztyTIdzRil6HXD', { api_host: 'https://app.posthog.com' });
+    posthog.init('phx_5dJFrBdgyLUbZ3H9YDpJyqBCe1Xo5TwOsztyTIdzRil6HXD', {
+  api_host: 'https://app.posthog.com',
+  session_recording: {}, // Enables session recording with default settings
+});
   }, []);
   return null;
 }
